47f87bddf3b0afdbf549bb009730af96cc9a29dd
Andy Agus <Redagus@gmail.com>
2020-07-19 17:37:08 -0400

    failed solution lol

---
+++ src/index.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+   
+    const url = "http://localhost:3000/pups"
+    const dogBar = document.querySelector("#dog-bar")
+    const dogInfo = document.querySelector("#dog-info")
+		var button = document.createElement("button")
+		var goodDogFilter = document.querySelector("#good-dog-filter")
+		console.dir(goodDogFilter)
+
+		const dogNameCard = document.createElement('div')    
+		let goodDog = false 
+
+		goodDogFilter.addEventListener("click", (e) => {
+			if(e.target.innerText.includes("OFF")){
+			goodDogFilter.textContent = "Filter good dogs: ON"
+			fetchDogs(goodDogFilter.textContent)
+			}else{
+			 goodDogFilter.textContent = "Filter good dogs: OFF"
+				fetchDogs(goodDogFilter.textContent)
+			}
+
+		}) 	
+
+    const fetchDogs = (a) => {
+
+	    if(goodDogFilter.textContent.includes("ON")){
+	    	fetch(url)
+	    	.then(resp => resp.json())  
+	    	.then(dogs => dogs.filter(dog => dog.isGoodDog == true))
+	    	.then(dogs => dogs.forEach(dog=> renderDogs(dog)))
+	   	}else{
+	    	fetch(url)	   		
+	   		.then(resp => resp.json())  
+	    	.then(dogs => dogs.forEach(dog=> renderDogs(dog)))
+	   	}
+		}
+
+
+
+
+
+		const renderDogs = (dog) => {
+			nameCard = document.createElement('div')
+			nameCard.innerHTML = `<span> ${dog.name} </span>`
+			dogBar.append(nameCard)
+
+
+			//listening on click
+			nameCard.addEventListener('click', (e) => {
+				dogNameCard.innerHTML = `
+				<img src=${dog.image}>
+ 				<h2>${dog.name}</h2>
+				`
+				dogNameCard.dataset.id = `${dog.id}`
+				button.innerHTML = "Good Dog"
+				button.className = "dog-button"
+				dogNameCard.append(button)
+				dogInfo.append(dogNameCard)
+
+				button.addEventListener('click', (e) => {
+					let newValue;
+					if(e.target.innerText.includes("Good")){
+						e.target.innerText = "Bad Dog"
+						newValue = false
+					}
+					else{
+						e.target.innerText = "Good Dog"
+						newValue = true;
+					}
+					
+					const id = dogNameCard.dataset.id
+					console.log(id) 
+					
+
+					fetch(`${url}/${id}`, {
+						method: "PATCH",
+						headers: {
+							"content-type": "application/json",
+							"accept": "application/json"
+						},
+						body: JSON.stringify({ isGoodDog: newValue })
+					})
+					.then(resp => resp.json(resp))
+					.then(resp => isGoodDog(resp))
+				})		
+			})
+
+			const isGoodDog = (resp) => {
+					if (resp.isGoodDog == true){
+						console.log("good")
+					}else{
+						console.log("bad")
+					}
+				}
+
+
+		}
+});
+
